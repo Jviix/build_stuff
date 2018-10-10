5033eacb4d26b76c746f34e79f391c504999ea9f
Jviix <33033405+Jviix@users.noreply.github.com>
2018-10-10 17:45:15 +0200

    Fix

--- picks.sh
+++ picks.sh
@@ -2,19 +2,19 @@
 
 cd device/samsung/hlte-common
 
-git fetch https://github.com/LineageOS/android_device_samsung_hlte-common refs/changes/30/231030/1 && git cherry-p$
+git fetch https://github.com/LineageOS/android_device_samsung_hlte-common refs/changes/30/231030/1 && git cherry-pick FETCH_HEAD
 
-git fetch https://github.com/LineageOS/android_device_samsung_hlte-common refs/changes/18/225618/7 && git cherry-p$
+git fetch https://github.com/LineageOS/android_device_samsung_hlte-common refs/changes/18/225618/7 && git cherry-pick FETCH_HEAD
 
-git fetch https://github.com/LineageOS/android_device_samsung_hlte-common refs/changes/19/225619/9 && git cherry-p$
+git fetch https://github.com/LineageOS/android_device_samsung_hlte-common refs/changes/19/225619/9 && git cherry-pick FETCH_HEAD
 
 cd && cd PixysOS/device/samsung/msm8974-common
 
-git fetch https://github.com/LineageOS/android_device_samsung_msm8974-common refs/changes/50/231350/1 && git cherr$
+git fetch https://github.com/LineageOS/android_device_samsung_msm8974-common refs/changes/50/231350/1 && git cherry-pick FETCH_HEAD
 
-git fetch https://github.com/LineageOS/android_device_samsung_msm8974-common refs/changes/77/228677/3 && git cherr$
+git fetch https://github.com/LineageOS/android_device_samsung_msm8974-common refs/changes/77/228677/3 && git cherry-pick FETCH_HEAD
 
-git fetch https://github.com/LineageOS/android_device_samsung_msm8974-common refs/changes/16/224916/12 && git cher$
+git fetch https://github.com/LineageOS/android_device_samsung_msm8974-common refs/changes/16/224916/12 && git cherry-pick FETCH_HEAD
 
 cd && cd PixysOS/bionic
 
@@ -40,25 +40,25 @@ git fetch https://github.com/LineageOS/android_dalvik refs/changes/76/225476/1 &
 
 cd && cd PixysOS/hardware/qcom/audio
 
-git fetch https://github.com/LineageOS/android_hardware_qcom_audio refs/changes/49/230749/1 && git cherry-pick FET$
+git fetch https://github.com/LineageOS/android_hardware_qcom_audio refs/changes/49/230749/1 && git cherry-pick FETCH_HEAD
 
 cd && cd PixysOS/hardware/qcom/audio-caf/msm8974
 
-git fetch https://github.com/LineageOS/android_hardware_qcom_audio refs/changes/36/223436/5 && git cherry-pick FET$
+git fetch https://github.com/LineageOS/android_hardware_qcom_audio refs/changes/36/223436/5 && git cherry-pick FETCH_HEAD
 
 cd && cd PixysOS/hardware/qcom/display-caf/msm8974
 
-git fetch https://github.com/LineageOS/android_hardware_qcom_display refs/changes/81/226481/2 && git cherry-pick F$
+git fetch https://github.com/LineageOS/android_hardware_qcom_display refs/changes/81/226481/2 && git cherry-pick FETCH_HEAD
 
-git fetch https://github.com/LineageOS/android_hardware_qcom_display refs/changes/82/226482/1 && git cherry-pick F$
+git fetch https://github.com/LineageOS/android_hardware_qcom_display refs/changes/82/226482/1 && git cherry-pick FETCH_HEAD
 
 cd && cd PixysOS/hardware/qcom/media
 
-git fetch https://github.com/LineageOS/android_hardware_qcom_media refs/changes/50/230750/2 && git cherry-pick FET$
+git fetch https://github.com/LineageOS/android_hardware_qcom_media refs/changes/50/230750/2 && git cherry-pick FETCH_HEAD
 
 cd && cd PixysOS/hardware/qcom/media-caf/msm8974
 
-git fetch https://github.com/LineageOS/android_hardware_qcom_media refs/changes/41/223441/1 && git cherry-pick FET$
+git fetch https://github.com/LineageOS/android_hardware_qcom_media refs/changes/41/223441/1 && git cherry-pick FETCH_HEAD
 
 cd && cd PixysOS/system/netd
 
@@ -66,8 +66,8 @@ git fetch https://github.com/LineageOS/android_system_netd refs/changes/01/23120
 
 cd && cd PixysOS/system/sepolicy
 
-git fetch https://github.com/LineageOS/android_system_sepolicy refs/changes/46/223746/6 && git cherry-pick FETCH_H$
+git fetch https://github.com/LineageOS/android_system_sepolicy refs/changes/46/223746/6 && git cherry-pick FETCH_HEAD
 
-git fetch https://github.com/LineageOS/android_system_sepolicy refs/changes/48/223748/6 && git cherry-pick FETCH_H$
+git fetch https://github.com/LineageOS/android_system_sepolicy refs/changes/48/223748/6 && git cherry-pick FETCH_HEAD
 
 cd && cd PixysOS 
