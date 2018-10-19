05b1860a332eedde63b01fad0cf740e69c90e355
Jviix <33033405+Jviix@users.noreply.github.com>
2018-10-20 01:03:57 +0200

    Update picks.sh

--- picks.sh
+++ picks.sh
@@ -6,7 +6,7 @@ source build/envsetup.sh
 
 # bionic
 losrepopick 230099 # Actually restore pre-P mutex behavior
-
+losrepopick 223067 # libc fortify: Ignore open() O_TMPFILE mode bits warning
 
 # build/make
 losrepopick 222760 # Add LOCAL_AIDL_FLAGS
@@ -37,8 +37,20 @@ losrepopick 228585 # sepolicy: Allow mm-qcamerad to access v4L "name" node
 losrepopick 228586 # common: Fix labelling of lcd-backlight
 losrepopick 228587 # sepolicy: Allow perf HAL to set freq props
 
+# device/qcom/sepolicy-legacy
+losrepopick 228608 # sepolicy-legacy: Treat BT vendor properties like non-vendor ones.
+losrepopick 230828 # legacy: Label more power_supply sysfs
+losrepopick 230829 # legacy: Resolve hal_gnss_default denial
+losrepopick 230830 # legacy: Resolve hal_bluetooth_default denial
+losrepopick 230831 # sepolicy: Allow android service to write on tombstones
+losrepopick 230833 # legacy: Correctly label display.qservice
+losrepopick 230834 # legacy: allow init to read /proc/device-tree
+losrepopick 230832 # Revert "sepol: hostapd is now hal_wifi_hostapd"
+losrepopick 231049 # Change to compile due to hostapd.te removal
+losrepopick 231054 # NFC: Add nfc data file context and rename property
 
-
+# external/perfetto
+losrepopick 223413 # perfetto_cmd: Resolve missing O_CREAT mode
 
 # system/sepolicy
 losrepopick 223746 # Add rules required for TARGET_HAS_LEGACY_CAMERA_HAL1
