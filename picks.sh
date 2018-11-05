055b4ee0bf032f4ccb521eb4854f4d4bc2e9f5d5
Jviix <33033405+Jviix@users.noreply.github.com>
2018-11-05 20:28:41 +0100

    Update picks.sh

--- picks.sh
+++ picks.sh
@@ -4,9 +4,6 @@ source build/envsetup.sh
 
 # -------------- PLATFORM STUFF --------------
 
-# bionic
-losrepopick 230099 # Actually restore pre-P mutex behavior
-
 # build/make
 losrepopick 222760 # Add LOCAL_AIDL_FLAGS
 losrepopick 222742 # build: Use project pathmap for recovery
@@ -33,23 +30,16 @@ losrepopick 228583 # sepolicy: allow vold to read persist dirs
 losrepopick 228584 # sepolicy: Fix video4linux "name" node labeling
 losrepopick 228585 # sepolicy: Allow mm-qcamerad to access v4L "name" node
 losrepopick 228586 # common: Fix labelling of lcd-backlight
-losrepopick 228587 # sepolicy: Allow perf HAL to set freq props
 
 # device/qcom/sepolicy-legacy
-losrepopick 228608 # sepolicy-legacy: Treat BT vendor properties like non-vendor ones.
 losrepopick 230828 # legacy: Label more power_supply sysfs
 losrepopick 230829 # legacy: Resolve hal_gnss_default denial
 losrepopick 230830 # legacy: Resolve hal_bluetooth_default denial
-losrepopick 230831 # sepolicy: Allow android service to write on tombstones
-losrepopick 230833 # legacy: Correctly label display.qservice
 losrepopick 230834 # legacy: allow init to read /proc/device-tree
-losrepopick 230832 # Revert "sepol: hostapd is now hal_wifi_hostapd"
-losrepopick 231049 # Change to compile due to hostapd.te removal
 losrepopick 231054 # NFC: Add nfc data file context and rename property
 losrepopick 230235 # common: grant DRM HIDL HAL ownership access to /data/{misc,vendor}/media/
 
 # system/sepolicy
-losrepopick 223748 # Build sepolicy tools with Android.bp.
 losrepopick 230151 # Fix storaged access to /sys/block/mmcblk0/stat after c936223c
 
 # -------------- TOPIC STUFF --------------
