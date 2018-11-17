a79e4a6c725a2f75d081baac47197d05813bd566
Jviix <33033405+Jviix@users.noreply.github.com>
2018-11-17 18:04:56 +0100

    Update picks.sh

--- picks.sh
+++ picks.sh
@@ -32,12 +32,16 @@ losrepopick 228585 # sepolicy: Allow mm-qcamerad to access v4L "name" node
 losrepopick 228586 # common: Fix labelling of lcd-backlight
 
 # device/qcom/sepolicy-legacy
+losrepopick 234248 # sepolicy : set write permissions for sysfs_boot_adsp.
 losrepopick 230828 # legacy: Label more power_supply sysfs
 losrepopick 230829 # legacy: Resolve hal_gnss_default denial
 losrepopick 230830 # legacy: Resolve hal_bluetooth_default denial
 losrepopick 230834 # legacy: allow init to read /proc/device-tree
 losrepopick 231054 # NFC: Add nfc data file context and rename property
 losrepopick 230235 # common: grant DRM HIDL HAL ownership access to /data/{misc,vendor}/media/
+
+# system/netd
+repopick 234190 # netd: Allow devices to opt-out of the tethering active FTP helper
 
 # system/sepolicy
 losrepopick 230151 # Fix storaged access to /sys/block/mmcblk0/stat after c936223c
