48ed32ecfade9a4d00852dd3f619596f8d8db35a
Jviix <33033405+Jviix@users.noreply.github.com>
2018-11-26 19:41:06 +0100

    Update picks.sh

--- picks.sh
+++ picks.sh
@@ -20,6 +20,7 @@ repopick 1261 # sepolicy: allow vold to read persist dirs
 repopick 1262 # sepolicy: Fix video4linux "name" node labeling
 repopick 1263 # sepolicy: Allow mm-qcamerad to access v4L "name" node
 repopick 1264 # common: Fix labelling of lcd-backlight
+repopick 1293 # sepolicy: Remove rules for no longer supported platforms
 
 # device/qcom/sepolicy-legacy
 repopick 1266 # sepolicy : set write permissions for sysfs_boot_adsp.
@@ -29,6 +30,10 @@ repopick 1269 # legacy: Resolve hal_bluetooth_default denial
 repopick 1270 # legacy: allow init to read /proc/device-tree
 repopick 1271 # NFC: Add nfc data file context and rename property
 repopick 1272 # common: grant DRM HIDL HAL ownership access to /data/{misc,vendor}/media/
+repopick 1289 # wifi: Use wpa_data_file instead of wifi_vendor_data_file
+repopick 1290 # sepolicy: Allow hostapd to access/write /data/vendor/ partition
+repopick 1291 # Add create_dir_perms to data files for vendor_init
+repopick 1292 # Remove wifi_data_file from data_between_core_and_vendor_violators
 
 # device/custom/sepolicy
 repopick 1273 # common: Expand labeling of sysfs_vibrator nodes using regex
@@ -41,5 +46,7 @@ repopick 1285 # netd: Allow devices to opt-out of the tethering active FTP helpe
 repopick 1283 # sepolicy: Grant mediaextractor access to files over all sdcard fs types
 repopick 1284 # Fix storaged access to /sys/block/mmcblk0/stat after c936223c
 repopick 1286 # Allow init to write to /proc/cpu/alignment
+repopick 1287 # sepolicy: Address denials for legacy last_kmsg file
+repopick 1288 # Allow init to chmod/chown /proc/slabinfo
 
 # -------------- TOPIC STUFF --------------
